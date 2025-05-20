201cf73bfc151c2fdff701a5804d8597715dd9d0
valechakamal <kamal.valecha@gmail.com>
2025-05-20 23:49:40 +0530

    New react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Website under construction
+      </header>
+    </div>
+  );
+}
+
+export default App;
